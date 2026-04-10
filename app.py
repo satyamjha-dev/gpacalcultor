6beb45250f77be16ae4e1b308710c0b367fd096c
Satyam Jha <jhaasatyamm@gmail.com>
2026-04-10 06:27:19 +0530

    Updating files

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ from models import db, User, Result, Review
 import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback-secret-key')
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(BASE_DIR, "database.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + db_path
+db_url = os.environ.get("DATABASE_URL")
+
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "pool_pre_ping": True,
+}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize extensions
